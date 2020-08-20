d11216f33163d986ac244dc08e43cdcb2a1d5fd1
Wakuba99160210627974 <1insp99persptashtash16@gmail.com>
2020-08-20 22:44:03 +0900

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import * as firebase from "firebase";
+import "firebase/firestore";
+import { StyleSheet, Text, View, SnapshotViewIOS } from "react-native";
+
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_MQDUVbrQ2muTjzEAwHs58ykeHFGzgZ0",
+    authDomain: "shop-review-64ee4.firebaseapp.com",
+    databaseURL: "https:/shop-review-64ee4firebaseio.com",
+    projectId: "shop-review-64ee4",
+    storageBucket: "shop-review-64ee4.appspot.com",
+    messagingSenderId: "177053344472",
+    appId: "1:177053344472:web:bf15723428d41773f06dad",
+    measurementId: "G-GX1WFWD62X",
+  };
+  firebase.initializeApp(firebaseConfig);
+}
+
+//TypeScriptで型を定義
+type Shop = {
+  name: string;
+  place: string;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [shops, setShops] = useState<Shop[]>([]); //初期値はカラの配列、<Shop[]>はTypeScriotの型を反映させる
+  useEffect(() => {
+    getFirebaseItems();
+  }, []); //第２引数を[]のようにカラにしておくとコンポーネント初期化時に一度だけ呼び出せる
+
+  const getFirebaseItems = async () => {
+    const snapshot = await firebase.firestore().collection("shops").get();
+    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    setShops(shops);
+    // console.log(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => (
+    <View style={{ margin: 10 }} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
     </View>
-  );
+  ));
+
+  return <View style={styles.container}>{shopItems}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
