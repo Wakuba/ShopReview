dd69c910187327add0e7ed12e7f1860b29277e6d
TashiroKyouichiro <1insp99persptashtash16+aenginei@gmail.com>
2020-08-22 16:52:56 +0900

    2-8

--- App.tsx
+++ App.tsx
@@ -1,28 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import * as firebase from "firebase";
-import "firebase/firestore";
-import { StyleSheet, Text, View, SnapshotViewIOS } from "react-native";
-
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyD_MQDUVbrQ2muTjzEAwHs58ykeHFGzgZ0",
-    authDomain: "shop-review-64ee4.firebaseapp.com",
-    databaseURL: "https:/shop-review-64ee4firebaseio.com",
-    projectId: "shop-review-64ee4",
-    storageBucket: "shop-review-64ee4.appspot.com",
-    messagingSenderId: "177053344472",
-    appId: "1:177053344472:web:bf15723428d41773f06dad",
-    measurementId: "G-GX1WFWD62X",
-  };
-  firebase.initializeApp(firebaseConfig);
-}
-
-//TypeScriptで型を定義
-type Shop = {
-  name: string;
-  place: string;
-};
+import { StyleSheet, Text, View } from "react-native";
+import { getShops } from "./src/lib/firebase";
+import { Shop } from "./src/lib/types/shop";
 
 export default function App() {
   const [shops, setShops] = useState<Shop[]>([]); //初期値はカラの配列、<Shop[]>はTypeScriotの型を反映させる
@@ -31,8 +10,7 @@ export default function App() {
   }, []); //第２引数を[]のようにカラにしておくとコンポーネント初期化時に一度だけ呼び出せる
 
   const getFirebaseItems = async () => {
-    const snapshot = await firebase.firestore().collection("shops").get();
-    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    const shops = await getShops();
     setShops(shops);
     // console.log(shops);
   };
---
+++ src/lib/firebase.ts
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import { Shop } from "./types/shop";
+import Constants from "expo-constants";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(Constants.manifest.extra.firebase);
+}
+
+export const getShops = async () => {
+  const snapshot = await firebase.firestore().collection("shops").get();
+  const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+  return shops;
+};
